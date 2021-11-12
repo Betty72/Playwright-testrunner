63d754d85aaf2310391cc32d6c5d66bdc86b4119
Betisa Teale <89780671+Betty72@users.noreply.github.com>
2021-11-12 13:38:13 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
           
         
     },
-    testMatch: ["tc001.test.ts"], //**.test.ts to run all the tests in the project
+    testMatch: ["console.test.ts"], //**.test.ts to run all the tests in the project
     retries: 0,
     reporter: [["dot"], ["json", {outputFile: "test-result.json"}]]
 }
