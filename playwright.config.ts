a1ef1c621727dcf54b3ece23be96cf09c845e691
Betisa Teale <89780671+Betty72@users.noreply.github.com>
2021-11-12 13:44:07 +0100

    new test

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ const config: PlaywrightTestConfig = {
 
     use: {
 
-        headless: false,
+        headless: true,
         channel: "chrome",
         screenshot: "only-on-failure",
         video: "retain-on-failure",
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
           
         
     },
-    testMatch: ["console.test.ts"], //**.test.ts to run all the tests in the project
+    testMatch: ["tc001.test.ts"], //**.test.ts to run all the tests in the project
     retries: 0,
     reporter: [["dot"], ["json", {outputFile: "test-result.json"}]]
 }
