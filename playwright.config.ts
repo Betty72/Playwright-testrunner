ab7a93b7280826baa157b32ee8579a7201dbae8e
Betisa Teale <89780671+Betty72@users.noreply.github.com>
2021-11-14 10:17:43 +0100

    updated tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
           
         
     },
-    testMatch: ["unibet.test.ts"], //**.test.ts to run all the tests in the project
+    testMatch: ["kambiplay.test.ts"], //**.test.ts to run all the tests in the project
     retries: 0,
     reporter: [["dot"], ["json", {outputFile: "test-result.json"}]]
 }
